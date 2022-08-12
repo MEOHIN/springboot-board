ef21a5c72b8fa87ec4c672c9c888f2bd48a5a148
meohin <meohin.meo@gmail.com>
2022-08-12 17:57:12 +0900

    Add Question entity

---
+++ src/main/java/com/mysite/springbootboard/Question.java
@@ -0,0 +1,25 @@
+package com.mysite.springbootboard;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+// @Entity 어노테이션은 클래스를 엔티티로 만들고, @Entity 어노테이션이 있어야 JPA 가 엔티티로 인식한다.
+@Entity
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+}

58f94971503d3b18a6b109685dfb04f09bcd316c
meohin <meohin.meo@gmail.com>
2022-08-15 18:00:04 +0900

    Add test to look up data by Questions entity ID

--- src/test/java/com/mysite/springbootboard/SpringBootBoardApplicationTests.java
+++ src/test/java/com/mysite/springbootboard/SpringBootBoardApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -17,15 +18,20 @@ class SpringBootBoardApplicationTests {
 
 
 	/**
-	 * 데이터베이스에 저장된 데이터를 조회하는 테스트다.
+	 * 데이터베이스에 저장된 데이터를 조회한다.
 	 */
 	@Test
 	void testJpa() {
-		List<Question> all = this.questionRepository.findAll();    // question 테이블에 저장된 모드 데이터를 조회
-		assertEquals(2, all.size());    // 저장된 데이터는 총 2건이므로 리스트의 사이즈도 2다.
-
-		Question q = all.get(0);    // 리스트의 첫번째 데이터를 조회
-		assertEquals("sbb 가 무엇인가요?", q.getSubject());    // 리스트의 첫번째 데이터의 제목을 비교
+		/*
+		 * findById 의 리턴 타입은 Optional 이다.
+		 * Optional 은 null 처리를 유연하게 처리하기 위해 사용하는 클래스다.
+		 * isPresent 로 null 이 아닌지를 확인한 후에 get 으로 실제 Question 객체 값을 얻어야 한다.
+		 */
+		Optional<Question> optionalQuestion = this.questionRepository.findById(1);
+		if (optionalQuestion.isPresent()) {    // 엔티티 객체가 not null 인지 확인
+			Question q = optionalQuestion.get();    // 실제 Question 엔티티 객체 값을 가져온다
+			assertEquals("sbb 가 무엇인가요?", q.getSubject());    // 리스트의 첫번째 데이터의 제목을 비교
+		}
 	}
 
 }

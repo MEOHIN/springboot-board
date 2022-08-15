2ed7f2bf50ca62c1fdc2def92cc715b949423fbb
meohin <meohin.meo@gmail.com>
2022-08-15 18:14:00 +0900

    Add test to look up Question table data by title

--- src/test/java/com/mysite/springbootboard/SpringBootBoardApplicationTests.java
+++ src/test/java/com/mysite/springbootboard/SpringBootBoardApplicationTests.java
@@ -22,16 +22,8 @@ class SpringBootBoardApplicationTests {
 	 */
 	@Test
 	void testJpa() {
-		/*
-		 * findById 의 리턴 타입은 Optional 이다.
-		 * Optional 은 null 처리를 유연하게 처리하기 위해 사용하는 클래스다.
-		 * isPresent 로 null 이 아닌지를 확인한 후에 get 으로 실제 Question 객체 값을 얻어야 한다.
-		 */
-		Optional<Question> optionalQuestion = this.questionRepository.findById(1);
-		if (optionalQuestion.isPresent()) {    // 엔티티 객체가 not null 인지 확인
-			Question q = optionalQuestion.get();    // 실제 Question 엔티티 객체 값을 가져온다
-			assertEquals("sbb 가 무엇인가요?", q.getSubject());    // 리스트의 첫번째 데이터의 제목을 비교
-		}
+		Question q = this.questionRepository.findBySubject("sbb 가 무엇인가요?");    // 제목으로 테이블 데이터 조회
+		assertEquals("sbb 가 무엇인가요?", q.getSubject());
 	}
 
 }

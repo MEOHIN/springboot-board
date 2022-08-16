f68af9d577e560be1eb134fb1a9e2ccc87d3bfb1
meohin <meohin.meo@gmail.com>
2022-08-16 17:51:43 +0900

    Add method to look up data by attribute value of Question entity

--- src/main/java/com/mysite/springbootboard/QuestionRepository.java
+++ src/main/java/com/mysite/springbootboard/QuestionRepository.java
@@ -9,4 +9,12 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
      * @return 입력받은 값으로 식별디는 엔티티를 반환한다.
      */
     Question findBySubject(String subject);
+
+    /**
+     * subject 값과 content 값으로 데이터를 조회한다.
+     * @param subject Question 엔티티의 속성 중 제목
+     * @param content Question 엔티티의 속성 중 내용
+     * @return 입력받은 값으로 식별디는 엔티티를 반환한다.
+     */
+    Question findBySubjectAndContent(String subject, String content);
 }

086aca7dcbddcf4fabdb6792549dbb85d052868b
meohin <meohin.meo@gmail.com>
2022-08-16 17:58:30 +0900

    Add test to look up Question table by subject and content

--- src/test/java/com/mysite/springbootboard/SpringBootBoardApplicationTests.java
+++ src/test/java/com/mysite/springbootboard/SpringBootBoardApplicationTests.java
@@ -4,10 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
@@ -16,14 +12,13 @@ class SpringBootBoardApplicationTests {
 	@Autowired
 	private QuestionRepository questionRepository;
 
-
 	/**
 	 * 데이터베이스에 저장된 데이터를 조회한다.
 	 */
 	@Test
 	void testJpa() {
-		Question q = this.questionRepository.findBySubject("sbb 가 무엇인가요?");    // 제목으로 테이블 데이터 조회
-		assertEquals("sbb 가 무엇인가요?", q.getSubject());
+		Question q = this.questionRepository
+				.findBySubjectAndContent("sbb 가 무엇인가요?", "sbb 에 대해서 알고 싶습니다.");
+		assertEquals(1, q.getId());
 	}
-
 }

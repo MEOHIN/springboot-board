fc7e488e1c8fc121f85f4f0b4e703b502cdc29fd
meohin <meohin.meo@gmail.com>
2022-08-17 17:46:37 +0900

    Add method to look up data with a specific string in subject

--- src/main/java/com/mysite/springbootboard/QuestionRepository.java
+++ src/main/java/com/mysite/springbootboard/QuestionRepository.java
@@ -2,11 +2,13 @@ package com.mysite.springbootboard;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
     /**
      * subject 값으로 데이터를 조회한다.
      * @param subject Question 엔티티의 속성
-     * @return 입력받은 값으로 식별디는 엔티티를 반환한다.
+     * @return 입력받은 값으로 식별되는 엔티티를 반환한다.
      */
     Question findBySubject(String subject);
 
@@ -14,7 +16,14 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
      * subject 값과 content 값으로 데이터를 조회한다.
      * @param subject Question 엔티티의 속성 중 제목
      * @param content Question 엔티티의 속성 중 내용
-     * @return 입력받은 값으로 식별디는 엔티티를 반환한다.
+     * @return 입력받은 값으로 식별되는 엔티티를 반환한다.
      */
     Question findBySubjectAndContent(String subject, String content);
+
+    /**
+     * subject 에 특정 문자열이 포함된 데이터를 조회한다.
+     * @param subject Question 엔티티의 속성 중 제목
+     * @return 입력받은 값으로 식별되는 엔티티 List 를 반환한다.
+     */
+    List<Question> findBySubjectLike(String subject);
 }

3a290e996c16e229b7e89a70fc3d67eaf77053ae
meohin <meohin.meo@gmail.com>
2022-08-18 13:59:23 +0900

    Add test to get Answer List from Question object

--- src/test/java/com/mysite/springbootboard/SpringBootBoardApplicationTests.java
+++ src/test/java/com/mysite/springbootboard/SpringBootBoardApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -19,13 +20,17 @@ class SpringBootBoardApplicationTests {
 	private AnswerRepository answerRepository;
 
 	/**
-	 * 데이터베이스에 저장된 id 가 1인 답변 데이터를 조회한다.
+	 * 질문 객체로부터 답변 리스트를 구한다.
 	 */
 	@Test
 	void testJpa() {
-		Optional<Answer> oa = this.answerRepository.findById(1);
-		assertTrue(oa.isPresent());
-		Answer a = oa.get();
-		assertEquals(2, a.getQuestion().getId());
+		Optional<Question> oq = this.questionRepository.findById(2);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+
+		List<Answer> answerList = q.getAnswerList();
+
+		assertEquals(1, answerList.size());
+		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
 	}
 }

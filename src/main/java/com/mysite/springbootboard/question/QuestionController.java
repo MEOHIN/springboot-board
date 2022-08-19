f872a8ed8fc20363b939df7c5855edc22247e0c4
meohin <meohin.meo@gmail.com>
2022-08-19 17:06:53 +0900

    Remove unnecessary docs

--- src/main/java/com/mysite/springbootboard/question/QuestionController.java
+++ src/main/java/com/mysite/springbootboard/question/QuestionController.java
@@ -7,14 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-/**
- * @RequiredArgsConstructor
- * 롬복이 제공하는 어노테이션
- * final 이 붙은 속성을 포함하는 생성자를 자동으로 생성
- *
- * 스프링 의존성 주입 규칙에 의해 questionRepository 객체가 자동으로 주입된다.
- * questionRepository 속성을 포함하는 생성자 생성한다.
- */
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {

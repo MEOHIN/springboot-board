0b1023c4b8a1c0eda2d88631bad9c19f48bba9d8
meohin <meohin.meo@gmail.com>
2022-08-19 18:55:47 +0900

    Add method that looks up and returns question list, but raises DataNotFoundException if there is no Question data

--- src/main/java/com/mysite/springbootboard/question/QuestionService.java
+++ src/main/java/com/mysite/springbootboard/question/QuestionService.java
@@ -1,9 +1,11 @@
 package com.mysite.springbootboard.question;
 
+import com.mysite.springbootboard.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -17,5 +19,19 @@ public class QuestionService {
      */
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+
+    /**
+     * id 값으로 Question 데이터를 조회한다.
+     * @param id Question PK
+     * @return 조회한 Question 데이터
+     */
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }

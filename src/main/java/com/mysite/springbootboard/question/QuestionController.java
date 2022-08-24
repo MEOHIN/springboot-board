1d221b83805e0d60593b1a3e7c194354f01118a9
meohin <meohin.meo@gmail.com>
2022-08-24 16:25:15 +0900

    Add common URL prefix to apply to class

--- src/main/java/com/mysite/springbootboard/question/QuestionController.java
+++ src/main/java/com/mysite/springbootboard/question/QuestionController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
+@RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
@@ -16,7 +17,7 @@ public class QuestionController {
 
     private final QuestionService questionService;
 
-    @RequestMapping("/question/list")
+    @RequestMapping("/list")
     public String list(Model model) {
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
@@ -24,9 +25,9 @@ public class QuestionController {
     }
 
     /**
-     * QuestionService 의 getQuestion 메서드를 호출하여 Question 객체를 템플릿에 전달한ㄴ다.
+     * QuestionService 의 getQuestion 메서드를 호출하여 Question 객체를 템플릿에 전달한다.
      */
-    @RequestMapping(value = "/question/detail/{id}")
+    @RequestMapping(value = "/detail/{id}")
     public String detail(Model model, @PathVariable("id") Integer id) {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);

a884b7c2ef4bb612396091888f0afa0abb47ada0
meohin <meohin.meo@gmail.com>
2022-08-25 11:30:33 +0900

    Add Answer controller

---
+++ src/main/java/com/mysite/springbootboard/answer/AnswerController.java
@@ -0,0 +1,25 @@
+package com.mysite.springbootboard.answer;
+
+import com.mysite.springbootboard.question.Question;
+import com.mysite.springbootboard.question.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequestMapping("/answer")
+@RequiredArgsConstructor
+@Controller
+public class AnswerController {
+
+    private final QuestionService questionService;
+
+    @PostMapping("/create/{id}")
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam String content) {
+        Question question = this.questionService.getQuestion(id);
+        return String.format("redirect:/question/detail/%s", id);
+    }
+}

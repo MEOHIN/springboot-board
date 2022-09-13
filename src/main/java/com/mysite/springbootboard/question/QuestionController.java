d0c04f4e541421a55dbbf472add99da8f998b08f
meohin <meohin.meo@gmail.com>
2022-09-13 17:47:48 +0900

    Add parameter to list method to get page value from URL requested by GET, and pass Page<Question> object paging to template

--- src/main/java/com/mysite/springbootboard/question/QuestionController.java
+++ src/main/java/com/mysite/springbootboard/question/QuestionController.java
@@ -2,13 +2,13 @@ package com.mysite.springbootboard.question;
 
 import com.mysite.springbootboard.answer.AnswerForm;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @RequestMapping("/question")
 @RequiredArgsConstructor
@@ -20,9 +20,9 @@ public class QuestionController {
     private final QuestionService questionService;
 
     @RequestMapping("/list")
-    public String list(Model model) {
-        List<Question> questionList = this.questionService.getList();
-        model.addAttribute("questionList", questionList);
+    public String list(Model model, @RequestParam(value = "page", defaultValue = "0") int page) {
+        Page<Question> paging = this.questionService.getList(page);
+        model.addAttribute("paging", paging);
        return "question_list";
     }
 

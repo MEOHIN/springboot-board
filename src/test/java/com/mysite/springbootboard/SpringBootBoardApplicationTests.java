22ef137dca63acc2401d133774c102edb8f0df4c
meohin <meohin.meo@gmail.com>
2022-09-13 17:11:52 +0900

    Add test case to generate 300 test data

--- src/test/java/com/mysite/springbootboard/SpringBootBoardApplicationTests.java
+++ src/test/java/com/mysite/springbootboard/SpringBootBoardApplicationTests.java
@@ -1,42 +1,45 @@
 package com.mysite.springbootboard;
 
-import com.mysite.springbootboard.answer.Answer;
-import com.mysite.springbootboard.answer.AnswerRepository;
-import com.mysite.springbootboard.question.Question;
-import com.mysite.springbootboard.question.QuestionRepository;
+import com.mysite.springbootboard.question.QuestionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import javax.transaction.Transactional;
-import java.util.List;
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 @SpringBootTest
 class SpringBootBoardApplicationTests {
+//
+//	@Autowired
+//	private QuestionRepository questionRepository;
+//
+//	@Autowired
+//	private AnswerRepository answerRepository;
+//
+//	/**
+//	 * 질문 객체로부터 답변 리스트를 구한다.
+//	 */
+//	@Transactional
+//	@Test
+//	void testJpa() {
+//		Optional<Question> oq = this.questionRepository.findById(2);
+//		assertTrue(oq.isPresent());
+//		Question q = oq.get();
+//
+//		List<Answer> answerList = q.getAnswerList();
+//
+//		assertEquals(1, answerList.size());
+//		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+//	}
+
 
 	@Autowired
-	private QuestionRepository questionRepository;
+	private QuestionService questionService;
 
-	@Autowired
-	private AnswerRepository answerRepository;
-
-	/**
-	 * 질문 객체로부터 답변 리스트를 구한다.
-	 */
-	@Transactional
 	@Test
 	void testJpa() {
-		Optional<Question> oq = this.questionRepository.findById(2);
-		assertTrue(oq.isPresent());
-		Question q = oq.get();
-
-		List<Answer> answerList = q.getAnswerList();
-
-		assertEquals(1, answerList.size());
-		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+		for (int i = 1; i <= 300; i++) {
+			String subject = String.format("테스트 데이터입니다:[%03d]", i);
+			String content = "내용무";
+			this.questionService.create(subject, content);
+		}
 	}
 }

b3fe2fb8fbc1c78b89a6675d6427dfbc4c596576
meohin <meohin.meo@gmail.com>
2022-09-15 17:57:50 +0900

    Remove unnecessary docs

--- src/main/java/com/mysite/springbootboard/SecurityConfig.java
+++ src/main/java/com/mysite/springbootboard/SecurityConfig.java
@@ -6,8 +6,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration    // 스프링의 환경설정 파일을 의미: 스프링 세큐리티의 설정을 위해 사용
-@EnableWebSecurity    // 모든 요청 URL 이 스프링 세큐리티의 제어를 받도록 사용: 내부적으로 SpringSecurityFilterChain이 동작하여 URL 필터 적용
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean

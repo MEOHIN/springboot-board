e860f83f6d0f88afdf0eea287681fc61cf47033c
meohin <meohin.meo@gmail.com>
2022-09-15 18:02:34 +0900

    Fix H2 console page corrupted error

--- src/main/java/com/mysite/springbootboard/SecurityConfig.java
+++ src/main/java/com/mysite/springbootboard/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 
 @Configuration
 @EnableWebSecurity
@@ -13,7 +14,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeRequests().antMatchers("/**").permitAll()
-                .and().csrf().ignoringAntMatchers("/h2-console/**");    // 모든 인증되지 않은 요청을 허락
+                .and()
+                .csrf().ignoringAntMatchers("/h2-console/**")
+                .and()
+                .headers().addHeaderWriter(new XFrameOptionsHeaderWriter(
+                        XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN));
         return httpSecurity.build();
     }
 }

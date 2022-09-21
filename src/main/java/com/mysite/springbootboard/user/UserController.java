f2c787701aa64e489cbbe78f399b574c947d217c
meohin <meohin.meo@gmail.com>
2022-09-21 17:23:16 +0900

    Add user controller for sign up

---
+++ src/main/java/com/mysite/springbootboard/user/UserController.java
@@ -0,0 +1,46 @@
+package com.mysite.springbootboard.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    /**
+     * /user/signup URL 이 GET 으로 요청되면 회원 가입을 위한 템플릿을 렌더링
+     */
+    @GetMapping("/signup")
+    public String signup(UserCreateForm userCreateForm){
+        return "signup_form";
+    }
+
+    /**
+     * /user/signup URL 이 POST로 요청되면 회원가입을 진행
+     */
+    public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult){
+        if (bindingResult.hasErrors()) {
+            return "signup_form";
+        }
+
+        if (!userCreateForm.getPassword1().equals(userCreateForm.getPassword2())) {
+            bindingResult.rejectValue("password2", "passwordInCorrect",
+                    "입력한 2개의 패스워트가 일치하지 않습니다.");
+            return "signup_form";
+        }
+
+        userService.create(userCreateForm.getUsername(),
+                userCreateForm.getEmail(),
+                userCreateForm.getPassword1());
+
+        return "redirect:/";
+    }
+}

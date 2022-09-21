1b1cd431f6f3b9755041c7d850323ca7ca5d4e8c
meohin <meohin.meo@gmail.com>
2022-09-21 17:00:52 +0900

    Update to inject and use PasswordEncoder object registered as bean

--- src/main/java/com/mysite/springbootboard/user/UserService.java
+++ src/main/java/com/mysite/springbootboard/user/UserService.java
@@ -1,14 +1,15 @@
 package com.mysite.springbootboard.user;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
 
-    private  final UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     /**
      * User 리파지토리를 사용해서 User 데이터를 생성한다.
@@ -17,8 +18,6 @@ public class UserService {
         SiteUser user = new SiteUser();
         user.setUsername(username);
         user.setEmail(email);
-
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         user.setPassword(passwordEncoder.encode(password));
         this.userRepository.save(user);
         return user;

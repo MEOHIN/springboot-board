481e083c17fe738bf44842a3d7329967bea5543c
meohin <meohin.meo@gmail.com>
2022-09-28 18:10:37 +0900

    Upgrade to save author when saving answer

--- src/main/java/com/mysite/springbootboard/answer/AnswerController.java
+++ src/main/java/com/mysite/springbootboard/answer/AnswerController.java
@@ -2,6 +2,8 @@ package com.mysite.springbootboard.answer;
 
 import com.mysite.springbootboard.question.Question;
 import com.mysite.springbootboard.question.QuestionService;
+import com.mysite.springbootboard.user.SiteUser;
+import com.mysite.springbootboard.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,16 +22,18 @@ public class AnswerController {
 
     private final QuestionService questionService;
     private final AnswerService answerService;
+    private final UserService userService;
 
     @PostMapping("/create/{id}")
     public String createAnswer(Model model, @PathVariable("id") Integer id,
                                @Valid AnswerForm answerForm, BindingResult bindingResult, Principal principal) {
         Question question = this.questionService.getQuestion(id);
+        SiteUser siteUser = this.userService.getUser(principal.getName());
         if (bindingResult.hasErrors()) {
             model.addAttribute("question", question);
             return "question_detail";
         }
-        this.answerService.create(question, answerForm.getContent());
+        this.answerService.create(question, answerForm.getContent(), siteUser);
         return String.format("redirect:/question/detail/%s", id);
     }
 }

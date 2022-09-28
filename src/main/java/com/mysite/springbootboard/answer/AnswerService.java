522a17e1ab27efe7794df674307ffc0ac7483345
meohin <meohin.meo@gmail.com>
2022-09-28 16:49:57 +0900

    Upgrade to get SiteUser object and set it in author attribute when saving answer

--- src/main/java/com/mysite/springbootboard/answer/AnswerService.java
+++ src/main/java/com/mysite/springbootboard/answer/AnswerService.java
@@ -1,6 +1,7 @@
 package com.mysite.springbootboard.answer;
 
 import com.mysite.springbootboard.question.Question;
+import com.mysite.springbootboard.user.SiteUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,11 +15,13 @@ public class AnswerService {
     /**
      * 답변 객체를 생성하고 저장한다.
      */
-    public void create(Question question, String content) {
+    public Answer create(Question question, String content, SiteUser author) {
         Answer answer = new Answer();
         answer.setContent(content);
         answer.setCreateDate(LocalDateTime.now());
         answer.setQuestion(question);
+        answer.setAuthor(author);
         this.answerRepository.save(answer);
+        return answer;
     }
 }

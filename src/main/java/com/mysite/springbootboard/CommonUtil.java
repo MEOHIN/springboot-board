03549d2cf11e56dd52a5d77a1c6f13ff0c1e437f
meohin <meohin.meo@gmail.com>
2022-10-05 17:12:19 +0900

    Add markdown component to apply markdown library to content of question or answer

---
+++ src/main/java/com/mysite/springbootboard/CommonUtil.java
@@ -0,0 +1,21 @@
+package com.mysite.springbootboard;
+
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommonUtil {
+    /**
+     * 마크다운 텍스트를 HTML 문서로 변환하여 리턴한다.
+     * @param markdown 마크다운 텍스트
+     * @return HTML 문서
+     */
+    public String markdown(String markdown) {
+        Parser parser = Parser.builder().build();
+        Node document = parser.parse(markdown);
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        return renderer.render(document);
+    }
+}
